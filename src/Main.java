a1140e3e3f991518d9fea15f5b9150bd09abe7ed
Pavel <146098297+rubanpavel2023@users.noreply.github.com>
2023-11-28 23:42:06 +0200

    added code for user input of personal and payment information

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import app.service.InfoService01;
+import app.service.InfoService02;
+
+public class Main {
+    public static void main(String[] args) {
+
+        InfoService01 infoService01 = new InfoService01();
+        String DataInfo01 = infoService01.getData();
+        System.out.println(DataInfo01);
+        System.out.println();
+
+        InfoService02 infoService02 = new InfoService02();
+        String DataInfo02 = infoService02.getData();
+        System.out.println(DataInfo02);
+    }
+}
